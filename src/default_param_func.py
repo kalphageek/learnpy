7f2b8a92520b6ce03fded3856f156a7187d12d15
kalphageek <kalphageek@outlook.com>
2019-07-13 14:31:37 +0900

    no message

--- src/default_param_func.py
+++ src/default_param_func.py
@@ -2,6 +2,7 @@ def connect_URI(server='localhost', port='80'):
     str = 'http://' + server + ':' + port
     return str
 
+#server는 default값 사용
 def main():
     sUri = connect_URI(port='8080')
     sUri = connect_URI()

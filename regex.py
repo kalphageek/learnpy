8a4da0a80386ea4b8b023995494fb0a0994a2e0e
정진덕 <kalphageek@outlook.com>
2019-09-15 20:08:31 +0900

    Update regex.py

--- regex.py
+++ regex.py
@@ -9,12 +9,12 @@ for phone in phone_regex.findall(text):
 #r' 는 문자를 문자 자체로 인식하도록 함
 #Flag -> re.I :대소문자 무시할때 사용, re.VERBOSE : 패턴이 길어지는 경우 사용
 #r'''는 Multi Line 문자열
-email_regex = re.compile(r'''(
+email_regex = re.compile(r'''
     ([\w\-.+]+)                 #group #1 - username
     @                           #@ symbol
-    [\w\-.]+(\.\w{2,4}){1,2}    #domain name
-    )''', re.VERBOSE)
+    ([\w\-.]+\.\w{2,4}){1,2}    #domain name
+    ''', re.VERBOSE)
 result = email_regex.search('My Email is kalphageek-01@outlook.com')
-print('Email : '+result.group())
+print('Email : '+result.group(0))
 print('Username : '+result.group(1))
 print('Domain : '+result.group(2))

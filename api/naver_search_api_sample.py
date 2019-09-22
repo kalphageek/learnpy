7a135452584776fd4a803f4b92aadcf007c7f502
kalphageek <kalphageek@outlook.com>
2019-09-22 09:16:07 +0900

    api update

--- api/naver_search_api_sample.py
+++ api/naver_search_api_sample.py
@@ -2,13 +2,13 @@ import requests
 import pprint
 
 headers = {
-    'X-Naver-Client-Id': 'TVcdGN3l5f8ZIswuZ4tX',
-    'X-Naver-Client-Secret': '4F9aWQDSdZ',
+    'X-Naver-Client-Id': '9qTwRVUXrl6d8KE_A9bw',
+    'X-Naver-Client-Secret': '4VAmjqZDkd',
 }
 
 payload = {
     'query': '파이썬',
-    'display': 100
+    'display': 5
 }
 
 url = 'https://openapi.naver.com/v1/search/blog'
@@ -16,6 +16,7 @@ url = 'https://openapi.naver.com/v1/search/blog'
 res = requests.get(url, headers=headers, params=payload)
 print('request sended..')
 
-result = res.json()['items'][2]['title']
+pprint.pprint(res.json())
 
+result = res.json()['items'][0]['title']
 print(result)

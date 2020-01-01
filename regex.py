2629ba5b8cc3d17dbbe8dbd91fb335a3d481cf0f
kalphageek <kalphageek@outlook.com>
2020-01-01 21:31:42 +0900

    regex test : split, match, sub, fullmatch, Flags

--- regex.py
+++ regex.py
@@ -5,9 +5,9 @@ text = 'My Number are home:031-705-4288, work:02-2324-8765'
 for phone in phone_regex.findall(text):
     print('Phone No. : (' + phone[1] + ')' + phone[2] + ' => ' + phone[0])
 
-#search()->첫번째 반환, findall()->모두 반환, split(), sub()->바꾸기
+#match()->패턴매치, search()->첫번째 반환, findall()->모두 반환, split()->, sub()->바꾸기
 #r' 는 문자를 문자 자체로 인식하도록 함
-#Flag -> re.I :대소문자 무시할때 사용, re.VERBOSE : 패턴이 길어지는 경우 사용
+#Flag -> re.I :대소문자 무시할때 사용, re.VERBOSE : 패턴이 길어지는 경우 사용-정규표현식에 주석을 달 수 있음
 #r'''는 Multi Line 문자열
 #(?: ... ) -> Non Capturing group
 #re.match("c", "abcdef")    # No match
@@ -16,7 +16,7 @@ email_regex = re.compile(r'''
     ([\w\-.]+)                 #group #1 - username
     @                           #@ symbol
     ([\w\-]+(?:\.\w{2,4}){1,2})    #domain name
-    ''', re.VERBOSE)
+    ''', re.VERBOSE|re.I)     # 다중 Flags 적용
 result = email_regex.search('My Email is kalphageek-01@outlook.co.kr.kr')
 print('Email : '+result.group(0))
 print('Username : '+result.group(1))
@@ -34,4 +34,8 @@ for entry in entries:
     items = re.split(":? ", entry)
     for item in items:
         print(item)
-        
+
+#[ ] 대괄호:여러 문자 중 하나와 일치
+matchObj = re.fullmatch("You[;']re studying re module[.,]", \
+                        'You\'re studying re module.')
+print(matchObj)
